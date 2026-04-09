6196f0fbe6e092bf5ccdf1639ed304ef87437b27
Avinash-bit06 <2400033236@kluniversity.in>
2026-04-09 14:11:30 +0530

    Remove Comments from coursecontroller

--- src/main/java/com/learnhub/backend/controller/CourseController.java
+++ src/main/java/com/learnhub/backend/controller/CourseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("*") // Open integration block
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/courses")
 @RequiredArgsConstructor
